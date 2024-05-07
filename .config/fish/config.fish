a3c92fb62bb5348567555fb898f94e72541083da
5-pebbles <5-pebble@protonmail.com>
2024-05-07 11:06:46 -0400

    feat(shell): add fish config;

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+set fish_greeting
+
+set TERM xterm-256color
+set EDITOR hx
+
+function fish_prompt
+    string join '' -- (set_color red) '[' (date '+%H:%M:%S') '] ' (set_color green) $PWD (set_color normal) (fish_git_prompt) ' ❯ '
+end
