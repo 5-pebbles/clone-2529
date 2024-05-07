43e8c83ee0e6a3dee4fc138cbbd17ed8ed4fef31
5-pebbles <5-pebble@protonmail.com>
2024-05-07 13:32:41 -0400

    fix(fish): add .cargo/bin to PATH;

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,8 @@ set fish_greeting
 set TERM xterm-256color
 set EDITOR hx
 
+fish_add_path $HOME/.cargo/bin
+
 function fish_prompt
     string join '' -- (set_color red) '[' (date '+%H:%M:%S') '] ' (set_color green) $PWD (set_color normal) ' ❯ '
 end
