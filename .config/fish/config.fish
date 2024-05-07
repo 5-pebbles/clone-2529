9096875341292661f81d3324e18c61d86de60ebd
5-pebbles <5-pebble@protonmail.com>
2024-05-07 11:11:33 -0400

    rm(git_prompt): rm fish git prompt;

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,5 +4,5 @@ set TERM xterm-256color
 set EDITOR hx
 
 function fish_prompt
-    string join '' -- (set_color red) '[' (date '+%H:%M:%S') '] ' (set_color green) $PWD (set_color normal) (fish_git_prompt) ' ❯ '
+    string join '' -- (set_color red) '[' (date '+%H:%M:%S') '] ' (set_color green) $PWD (set_color normal) ' ❯ '
 end
