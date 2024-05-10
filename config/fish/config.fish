1ee43a241a27793d9c666b7220c1dde11dc13632
5-pebbles <5-pebble@protonmail.com>
2024-05-10 11:31:34 -0400

    fix(shell): rm indent from greeting & prompt;

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,5 @@
 function fish_greeting
-    echo (set_color red) (date '+%A %d/%m/%y %H:%M:%S') (set_color normal)
+    string join '' -- (set_color red) (date '+%A %d/%m/%y %H:%M:%S') (set_color normal)
 end
 
 set TERM xterm-256color
@@ -8,5 +8,5 @@ set EDITOR hx
 fish_add_path $HOME/.cargo/bin
 
 function fish_prompt
-    string join '' -- ' ' (set_color green) (prompt_pwd) (set_color normal) ' ❯ '
+    string join '' -- (set_color green) (prompt_pwd) (set_color normal) ' ❯ '
 end
